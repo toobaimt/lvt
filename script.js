9836ee3be69d588bd01f5e0832fd899fcbaefb47
Tooba Imtiaz <14beetimtiaz@seecs.edu.pk>
2025-09-18 20:25:31 -0500

    added vid comparisons

--- script.js
+++ script.js
@@ -2,108 +2,138 @@
 var VIDEO_ASPECT_RATIO = 4.0 / 3.;
 
 var textStrings = [
-    "<em>Quark+</em>",
-    "<em>Quark</em>",
-    "<em>ENeRF</em>",
-    "<em>SIMPLI</em>",
-    "<em>GPNR</em>",
-    "<em>Quark+ (depth)</em>",
-    "<em>Quark (depth)</em>",
+    "<em>LVT<sub>SH-rgba</sub></em>",
+    "<em>LVT<sub>base</sub></em>",
+    "<em>Long-LRM</em>",
+    "<em>3DGS</em>",
+    "<em>LVT<sub>SH-rgba</sub> (depth)</em>",
+    "<em>LVT<sub>base</sub> (depth)</em>",
 ];
 
-$("#tools-video").on('loadedmetadata', function() {
+var textStrings_train = [
+    "<em>LVT<sub>SH-rgba</sub></em>",
+    "<em>LVT<sub>base</sub></em>",
+    "<em>3DGS</em>",
+    "<em>LVT<sub>SH-rgba</sub> (depth)</em>",
+    "<em>LVT<sub>base</sub> (depth)</em>",
+];
+
+$("#train-video").on('loadedmetadata', function() {
     this.width = this.videoWidth;
     this.height = this.videoHeight;
     console.log(this.width, this.height);
   });
 
 $(function() {
-    current_trex_idx = 0;
-    current_tools_idx = 0;
-    current_welder_idx = 0;
+    current_train_idx = 0;
+    current_greenhouse_idx = 0;
+    current_building_idx = 0;
+    current_temple_idx = 0;
 
-    trexVideo = document.getElementById('trex-video');
-    toolsVideo = document.getElementById('tools-video');
-    welderVideo = document.getElementById('welder-video');
+    trainVideo = document.getElementById('train-video');
+    greenhouseVideo = document.getElementById('greenhouse-video');
+    buildingVideo = document.getElementById('building-video');
+    templeVideo = document.getElementById('temple-video');
 
-    trexText = document.getElementById('trex-text');
-    toolsText = document.getElementById('tools-text');
-    welderText = document.getElementById('welder-text');
+    trainText = document.getElementById('train-text');
+    greenhouseText = document.getElementById('greenhouse-text');
+    buildingText = document.getElementById('building-text');
+    templeText = document.getElementById('temple-text');
 
-    trexThumbnails = [
-        document.getElementById('trex_quarkHQ'),
-        document.getElementById('trex_quark'),
-        document.getElementById('trex_enerf'),
-        document.getElementById('trex_simpli'),
-        document.getElementById('trex_gpnr'),
-        document.getElementById('trex_quarkHQ_depth'),
-        document.getElementById('trex_quark_depth'),
+    trainThumbnails = [
+        document.getElementById('lvtSH-train'),
+        document.getElementById('lvt-base-train'),
+        document.getElementById('3dgs-train'),
+        document.getElementById('lvtSH-train_depth'),
+        document.getElementById('lvt-base-train_depth'),
       ];
-      for (var i = 0; i < trexThumbnails.length; i++) {
-        trexThumbnails[i].addEventListener('click', change_trex_index.bind(this, i));
+      for (var i = 0; i < trainThumbnails.length; i++) {
+        trainThumbnails[i].addEventListener('click', change_train_index.bind(this, i));
       }
-      change_trex_index(current_trex_idx);
+      change_train_index(current_train_idx);
 
-    toolsThumbnails = [
-        document.getElementById('tools_quarkHQ'),
-        document.getElementById('tools_quark'),
-        document.getElementById('tools_enerf'),
-        document.getElementById('tools_simpli'),
-        document.getElementById('tools_gpnr'),
-        document.getElementById('tools_quark_depth'),
-        document.getElementById('tools_quarkHQ_depth'),
+    greenhouseThumbnails = [
+        document.getElementById('lvtSH-greenhouse'),
+        document.getElementById('lvt-base-greenhouse'),
+        document.getElementById('llrm-greenhouse'),
+        document.getElementById('3dgs-greenhouse'),
+        document.getElementById('lvtSH-greenhouse_depth'),
+        document.getElementById('lvt-base-greenhouse_depth'),
       ];
-      for (var i = 0; i < toolsThumbnails.length; i++) {
-        toolsThumbnails[i].addEventListener('click', change_tools_index.bind(this, i));
+      for (var i = 0; i < greenhouseThumbnails.length; i++) {
+        greenhouseThumbnails[i].addEventListener('click', change_greenhouse_index.bind(this, i));
       }
-      change_tools_index(current_tools_idx);
+      change_greenhouse_index(current_greenhouse_idx);
 
-
-      welderThumbnails = [
-        document.getElementById('welder_quarkHQ'),
-        document.getElementById('welder_quark'),
-        document.getElementById('welder_enerf'),
-        document.getElementById('welder_simpli'),
-        document.getElementById('welder_gpnr'),
-        document.getElementById('welder_quarkHQ_depth'),
-        document.getElementById('welder_quark_depth'),
+    buildingThumbnails = [
+        document.getElementById('lvtSH-building'),
+        document.getElementById('lvt-base-building'),
+        document.getElementById('llrm-building'),
+        document.getElementById('3dgs-building'),
+        document.getElementById('lvtSH-building_depth'),
+        document.getElementById('lvt-base-building_depth'),
       ];
-      for (var i = 0; i < welderThumbnails.length; i++) {
-        welderThumbnails[i].addEventListener('click', change_welder_index.bind(this, i));
+      for (var i = 0; i < buildingThumbnails.length; i++) {
+        buildingThumbnails[i].addEventListener('click', change_building_index.bind(this, i));
       }
-      change_welder_index(current_welder_idx);
+      change_building_index(current_building_idx);
+
+
+    templeThumbnails = [
+        document.getElementById('lvtSH-temple'),
+        document.getElementById('lvt-base-temple'),
+        document.getElementById('llrm-temple'),
+        document.getElementById('3dgs-temple'),
+        document.getElementById('lvtSH-temple_depth'),
+        document.getElementById('lvt-base-temple_depth'),
+      ];
+      for (var i = 0; i < templeThumbnails.length; i++) {
+        templeThumbnails[i].addEventListener('click', change_temple_index.bind(this, i));
+      }
+      change_temple_index(current_temple_idx);
 
   });
   
-function change_trex_index (idx) {
-    trexThumbnails[idx].classList.add("active-btn");
-    if (current_trex_idx != idx) {
-        trexThumbnails[current_trex_idx].classList.remove("active-btn");
+function change_train_index (idx) {
+    trainThumbnails[idx].classList.add("active-btn");
+    if (current_train_idx != idx) {
+        trainThumbnails[current_train_idx].classList.remove("active-btn");
     }
-    current_trex_idx = idx;
-    trexText.innerHTML = textStrings[idx];
-    trexVideo.src = "results/video_results/" + trexThumbnails[idx].id + ".mp4";
-    trexVideo.load();
+    current_train_idx = idx;
+    trainText.innerHTML = textStrings_train[idx];
+    trainVideo.src = "./lvt-files/results/video-results/" + trainThumbnails[idx].id + ".mp4";
+    trainVideo.load();
 }
 
-function change_tools_index (idx) {
-    toolsThumbnails[idx].classList.add("active-btn");
-    if (current_tools_idx != idx) {
-        toolsThumbnails[current_tools_idx].classList.remove("active-btn");
+function change_greenhouse_index (idx) {
+    greenhouseThumbnails[idx].classList.add("active-btn");
+    if (current_greenhouse_idx != idx) {
+        greenhouseThumbnails[current_greenhouse_idx].classList.remove("active-btn");
     }
-    current_tools_idx = idx;
-    toolsText.innerHTML = textStrings[idx];
-    toolsVideo.src = "results/video_results/" + toolsThumbnails[idx].id + ".mp4";
-    toolsVideo.load();
+    current_greenhouse_idx = idx;
+    greenhouseText.innerHTML = textStrings[idx];
+    greenhouseVideo.src = "./lvt-files/results/video-results/" + greenhouseThumbnails[idx].id + ".mp4";
+    greenhouseVideo.load();
 }
 
-function change_welder_index (idx) {
-    welderThumbnails[idx].classList.add("active-btn");
-    if (current_welder_idx != idx) {
-        welderThumbnails[current_welder_idx].classList.remove("active-btn");
+function change_building_index (idx) {
+    buildingThumbnails[idx].classList.add("active-btn");
+    if (current_building_idx != idx) {
+        buildingThumbnails[current_building_idx].classList.remove("active-btn");
     }
-    current_welder_idx = idx;
-    welderText.innerHTML = textStrings[idx];
-    welderVideo.src = "results/video_results/" + welderThumbnails[idx].id + ".mp4";
-    welderVideo.load();
+    current_building_idx = idx;
+    buildingText.innerHTML = textStrings[idx];
+    buildingVideo.src = "./lvt-files/results/video-results/" + buildingThumbnails[idx].id + ".mp4";
+    buildingVideo.load();
+}
+
+function change_temple_index (idx) {
+    templeThumbnails[idx].classList.add("active-btn");
+    if (current_temple_idx != idx) {
+        templeThumbnails[current_temple_idx].classList.remove("active-btn");
+    }
+    current_temple_idx = idx;
+    templeText.innerHTML = textStrings[idx];
+    templeVideo.src = "./lvt-files/results/video-results/" + templeThumbnails[idx].id + ".mp4";
+    templeVideo.load();
 }
